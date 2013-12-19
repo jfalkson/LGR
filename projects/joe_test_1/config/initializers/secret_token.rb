5debec658f0fa0d45875403c392945f64433c29c
jfalkson <jfalkson@gmail.com>
2013-12-18 19:39:50 -0500

    test

---
+++ projects/joe_test_1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoeTest1::Application.config.secret_token = '9b827bf7388962eb5e1d36961ae152f211add54cde9a58a378989cd8bd5d9b6f7b601486d0e8ae1cea9a71a30a54467f05d10015472f14e1dc6d516630fd1644'
